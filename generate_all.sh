c060ba89b3a9042269b8cc5723c6d8310f1c4e4f
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-10-27 16:58:51 +0100

    generate_all.sh - no need for trailing slashes, remove

--- generate_all.sh
+++ generate_all.sh
@@ -4,15 +4,17 @@ PATH1="ess_vocabs"
 PATH2="ess_vocabs"
 LANGS="en es pt fr it ko"
 
-declare -a TTLINPUTS=("${PATH1}/essglobal-vocab.ttl" \
-        "${PATH2}/activities.ttl" \
-        "${PATH2}/activities-modified.ttl" \
-        "${PATH2}/activities-ica.ttl" \
-        "${PATH2}/legal-form.ttl" \
-        "${PATH2}/organisational-structure.ttl" \
-        "${PATH2}/base-membership-type.ttl" \
-        "${PATH2}/qualifiers.ttl" \
-        "${PATH2}/type-of-labour.ttl")
+declare -a TTLINPUTS=(
+    "${PATH1}/essglobal-vocab.ttl"
+    "${PATH2}/activities.ttl"
+    "${PATH2}/activities-modified.ttl"
+    "${PATH2}/activities-ica.ttl"
+    "${PATH2}/legal-form.ttl"
+    "${PATH2}/organisational-structure.ttl"
+    "${PATH2}/base-membership-type.ttl"
+    "${PATH2}/qualifiers.ttl"
+    "${PATH2}/type-of-labour.ttl"
+)
 
 for lang in $LANGS; do
     ruby bin/goth "${TTLINPUTS[@]}" -l "$lang" > "essglobal-vocab_$lang.html"
