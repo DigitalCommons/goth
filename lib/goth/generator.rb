f54297baf92f94272e1b9129fc24175a70277901
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-10-27 09:31:21 +0100

    lib/goth/generator.rb - remove copy_assets
    This is no longer needed now we have removed all the local web assets
    from the template.

--- lib/goth/generator.rb
+++ lib/goth/generator.rb
@@ -15,18 +15,7 @@ module GOTH
       end
     end
 
-    def copy_assets #copy .js and .css assets to working directory
-      #asset_dir = File.join( File.dirname( __FILE__ ), "assets" )
-      asset_dir = File.join(File.dirname(__FILE__), "../..", "assets")
-      Dir.new(asset_dir).each() do |file|
-        if file != "." and file != ".."
-          FileUtils.cp( File.join(asset_dir, file), Dir.pwd )
-        end
-      end
-    end
-
     def run()
-      copy_assets()
       b = binding #an option for result
       schema = @schema
       introduction = @introduction
